a622d0bee1a723a5991d55262178aa38b806a203
LewisCM14 <Lewiscmartin@live.co.uk>
2022-09-04 14:47:35 +0100

    add Prompt to QuoteForm

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,15 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+    const history = useHistory(); // collect the history object
+
     const addQuoteHandler = quoteData => {
         console.log(quoteData);
+
+        // navigates to the /quotes page after submit
+        history.push('/quotes');
     }
 
     return <QuoteForm onAddQuote={addQuoteHandler} />;
